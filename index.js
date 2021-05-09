8fbee35bf7d124374ee550137f2323b4c50f39b8
Ayush <ayushmodi2003@gmail.com>
2021-05-10 00:56:02 +0530

    Add timestamp functionality

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
+//require('dotenv').config();
 const {ethers} = require('ethers');
 const fs = require('fs');
 const stringSimilarity = require("string-similarity");
+const EthDater = require('ethereum-block-by-date');
+const Web3 = require('web3');
 
 let bytecode = fs.readFileSync('bytecode.txt', 'utf8');
 
 
-async function main(blockNum, rpcURL) {
+async function detect(time, rpcURL) {
 
 	const provider = ethers.getDefaultProvider(rpcURL);
 
 	let latestBlock = await provider.getBlockNumber();
 
-	for (i = blockNum; i <= latestBlock; i ++) {
+	const dater = new EthDater(
+		new Web3(rpcURL)    
+	);
+	let block = await dater.getDate(
+		time, // Date, required. Any valid moment.js value: string, milliseconds, Date() object, moment() object.
+		true // Block after, optional. Search for the nearest block before or after the given date. By default true.
+	)
+	let fromBlock = block.block;
+
+	for (i = fromBlock; i <= latestBlock; i ++) {
 		let result = await provider.getBlockWithTransactions(
 		i)
 
@@ -28,4 +40,17 @@ async function main(blockNum, rpcURL) {
 }
 
 
-main(10202112, 'ropsten').then(() => process.exit(0));
+//detect('2021-05-09T13:20:40Z', "https://rinkeby-light.eth.linkpool.io").then(() => process.exit(0));
+
+async function main() {
+	networks = ['https://main-light.eth.linkpool.io', 'https://bsc-dataseed.binance.org/', 'https://rinkeby-light.eth.linkpool.io'];
+
+	for (n=0; n < networks.length; n++) {
+		console.log("analyzing", networks[n]);
+		
+		// Date, required. Any valid moment.js value: string, milliseconds, Date() object, moment() object
+		await detect('2021-05-09T13:20:40Z', networks[n]);
+	}
+}
+
+main()
